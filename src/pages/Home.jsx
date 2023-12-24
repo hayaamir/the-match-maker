8fed2b4d42786080ff81375597f763aca1144d0e
haya <hmn7707@gmail.com>
2023-12-24 22:54:46 +0200

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,50 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    return (
-        <div>
-            <h1 className={'text-white'}>Home page</h1>
-            <ul>
-                <li>The vision of the project</li>
-                <li>A brief explanation of the service</li>
-                <li>successes (when there are)...</li>
-                <li>Registration and login buttons</li>
-                <li>Contact Us Form</li>
-            </ul>
+  return (
+    <div>
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
         </div>
-    );
+        <div className="mx-auto max-w-2xl py-14">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              השדכנית
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              הכירי את הכלי שיעזור לך לנהל את השידוכים בקלות וביעילות. השדכנית
+              תאגד לך את כל כרטיסיות המשודכים שלך במקום אחד, שלחי אל המשודכים את
+              שאלון ההתאמה הפרטי שיתממשק אל מאגר הנתונים שלנו אוטומאטית וכך
+              תוכלי לנהל ביעילות את השידוכים
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                to="/registration"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                הרשמה
+              </Link>
+              <a
+                href="https://github.com/hayaamir/the-match-maker.git"
+                target="-blank"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                פרטים נוספים <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
